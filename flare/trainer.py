bb8f1cdb06db7e671b7aa6fa62f65fdb50dfc48b
Luiz Felix <lzcfelix@gmail.com>
2019-03-12 21:49:34 -0300

    Adding draft version of DataLoader trainer

--- flare/trainer.py
+++ flare/trainer.py
@@ -5,12 +5,89 @@ import numpy as np
 
 from torch.optim import Optimizer
 from torch import nn
+from torch.utils.data import DataLoader
 from sklearn import metrics
 
 from flare.callbacks import Callback, CallbacksContainer, ProgressBar
 from flare.history import ModelHistory
 
 _ceil = lambda x: int(np.ceil(x))
+
+def train_on_loader(model: nn.Module,
+                    train_gen: DataLoader,
+                    val_gen: Optional[DataLoader],
+                    loss_fn: Any,
+                    optimizer: Optimizer,
+                    n_epochs: int,
+                    callbacks: Optional[List[Callback]] = None) -> ModelHistory:
+    """
+    Helper function to train a model using torch DataLoader.
+
+    # Arguments
+        model: The PyTorch model.
+        train_gen: A DataLoader containing the training data.
+        val_gen: A DataLoader containing the validation data.
+        loss_fn: The loss function from which gradients are computed.
+        optimizer: The optimizer used in the backpropagation step.
+        n_epochs: How many passes should be performed over the train_gen.
+        callbacks: List of utility callbacks to help training the model.
+
+    # Return
+        A ModelHistory object representing the model training history.
+    """
+
+    callbacks_container = CallbacksContainer(callbacks or [])
+    callbacks_container.append(ProgressBar(len(train_gen), n_epochs))
+
+    model_history = ModelHistory(model)
+    for epoch in range(1, n_epochs + 1):
+        model.train()
+        callbacks_container.on_epoch_begin(epoch, model_history)
+
+        for batch_id, (X_trn, y_trn) in enumerate(train_gen):
+            callbacks_container.on_batch_begin(batch_id, model_history)
+
+            optimizer.zero_grad()
+            output = model(X_trn)
+            loss = loss_fn(output, y_trn)
+            loss.backward()
+            optimizer.step()
+
+            train_accuracy = metrics.accuracy_score(output.argmax(-1), y_trn)
+
+            model_history.append_trn_logs('loss', loss.item())
+            model_history.append_trn_logs('accuracy', train_accuracy)
+
+            callbacks_container.on_batch_end(batch_id, model_history)
+
+        if not val_gen:
+            continue
+
+        model.eval()
+        with torch.no_grad():
+            correct = 0
+            loss = 0
+            for batch_id, (X_dev, y_val) in enumerate(val_gen):
+                output = model(X_dev)
+
+                y_hat = output.argmax(-1)
+                correct += torch.sum(y_hat == y_val).item()
+                loss += loss_fn(output, y_val).item()
+
+            n_samples = len(val_gen.dataset)
+            model_history.append_dev_logs('val_loss', loss / n_samples)
+            model_history.append_dev_logs('val_accuracy', correct / n_samples)
+        model.train()
+
+        callbacks_container.on_epoch_end(epoch, model_history)
+        if model_history.should_stop_training():
+            break
+
+    model_history.close(n_epochs)
+    callbacks_container.on_train_end()
+
+    return model_history
+
 
 def train(model: nn.Module,
           X: torch.Tensor,
@@ -41,7 +118,6 @@ def train(model: nn.Module,
 
     if batch_size < 1:
         raise RuntimeError('Each batch should have at least one sample.')
-    callbacks_container = CallbacksContainer(callbacks or [])
 
     # first dimension in the amount of samples
     n_samples = X.shape[0]
@@ -63,8 +139,8 @@ def train(model: nn.Module,
     X_trn = X[val_samples:]
     y_trn = y[val_samples:]
 
-    ticker = ProgressBar(n_batches, n_epochs)
-    callbacks_container.append(ticker)
+    callbacks_container = CallbacksContainer(callbacks or [])
+    callbacks_container.append(ProgressBar(n_batches, n_epochs))
 
     model_history = ModelHistory(model)
     for epoch in range(1, n_epochs + 1):
@@ -109,8 +185,6 @@ def train(model: nn.Module,
 
         # computing loss/acc over the entire training / validation fold
         with torch.no_grad():
-            class_scores = model(X_trn)
-
             val_logits = model(X_val)
             val_preds = val_logits.argmax(-1)
             val_accuracy = metrics.accuracy_score(val_preds, y_val)
