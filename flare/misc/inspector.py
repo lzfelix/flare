2c2cd4c27f6a3f0ca11d035e4c7dd0f756b864bd
Luiz Felix <lzcfelix@gmail.com>
2019-07-20 20:15:07 -0300

    Supporting multiple input models and custom inspection

--- flare/misc/inspector.py
+++ flare/misc/inspector.py
@@ -1,14 +1,23 @@
 from collections import OrderedDict
-from typing import Union, List
+from typing import Union, List, Tuple, Optional
+import inspect
 
 import torch
 from torch import nn
 from torch.nn.utils import rnn as rnn_utils
 
 
-def summarize(model: nn.Module, in_tensor: Union[List[torch.Tensor], torch.Tensor]) -> None:
+def summarize(model: nn.Module,
+              in_tensor: Union[List[torch.Tensor], torch.Tensor],
+              inspect_custom: bool = False) -> None:
     module_stack = OrderedDict()
     all_hook_handles = list()
+
+    _filter_modules = lambda pair: pair[0][:2] != '__' and str(pair[1])[:6] == '<class'
+    default_modules = set(map(
+            lambda x: x[1],  # get just the module class, not its name
+            filter(_filter_modules, inspect.getmembers(nn.modules))
+        ))
 
     def get_shape(output: Union[torch.Tensor, rnn_utils.PackedSequence]):
         if isinstance(output, rnn_utils.PackedSequence):
@@ -27,13 +36,15 @@ def summarize(model: nn.Module, in_tensor: Union[List[torch.Tensor], torch.Tenso
             layer_name = str(module.__class__).split('.')[-1].split("'")[0]
             layer_name = f'{layer_idx}_{layer_name}'
 
-            # Modules usually have 1 input and 1 or more outputs
-            in_shape = get_shape(in_[0])
+            if isinstance(in_, (tuple, list)):
+                in_shape = list(map(get_shape, in_))
+            else:
+                in_shape = get_shape(in_)
 
             if isinstance(out_, (tuple, list)):
                 out_shape = list(map(get_shape, out_))
             else:
-                out_shape = get_shape(out_)
+                out_shape = [get_shape(out_)]
 
             trainable_params = sum(p.numel() for p in module.parameters() if p.requires_grad)
             non_trainable = sum(p.numel() for p in module.parameters() if not p.requires_grad)
@@ -44,18 +55,18 @@ def summarize(model: nn.Module, in_tensor: Union[List[torch.Tensor], torch.Tenso
                 'non_trainable': non_trainable
             }
 
-        if not module == model:
-            hook_handle = module.register_forward_hook(hook)
-            all_hook_handles.append(hook_handle)
+        hook_handle = module.register_forward_hook(hook)
+        all_hook_handles.append(hook_handle)
 
-    def format_out_shape(plist):
-        prefix = '\n' + ' ' * 71
-        if isinstance(plist, list):
-            return prefix.join([str(x) for x in plist])
+    # model.apply(register_hook)
+    for child_module in model.children():
+        if child_module == model:
+            continue
+        if not inspect_custom and child_module.__class__ not in default_modules:
+            register_hook(child_module)
         else:
-            return str(plist)
+            child_module.apply(register_hook)
 
-    model.apply(register_hook)
     with torch.no_grad():
         model(*in_tensor)
 
@@ -64,21 +75,37 @@ def summarize(model: nn.Module, in_tensor: Union[List[torch.Tensor], torch.Tenso
         handle.remove()
 
     # Formatting the output
-    print('=' * 90)
+    print('_' * 90)
     print('{:20}  {:22} {:25} {:15}'.format('Layer type',
                                             'Trainable / Frozen',
                                             'Input shape',
                                             'Output shape'))
-    print('-' * 90)
-    for name, specs in module_stack.items():
-        line_new = "{:20}  {:10} {:10}  {:25} {:15}".format(
+    print('=' * 90)
+    for i, (name, specs) in enumerate(module_stack.items()):
+        in_shapes = specs['in_shape']
+        out_shapes = specs['out_shape']
+
+        print('{:20}  {:10} {:10}  {:25} {:15}'.format(
             name,
             str(specs['trainable']),
             str(specs['non_trainable']),
-            str(specs['in_shape']),
-            format_out_shape(specs['out_shape'])
-        )
-        print(line_new)
+            str(in_shapes[0]),
+            str(out_shapes[0])
+        ))
+
+        longest_seq = max(len(in_shapes), len(out_shapes))
+        for seq_idx in range(1, longest_seq):
+            left_padding = [' ' * 45]
+            if seq_idx < len(in_shapes):
+                fmt_in = f'{str(in_shapes[seq_idx]):25} '
+            else:
+                fmt_in = ' ' * 26
+            left_padding.append(fmt_in)
+
+            if seq_idx < len(out_shapes):
+                left_padding.append(f'{str(out_shapes[seq_idx]):25}')
+
+            print(''.join(left_padding))
     print('=' * 90)
 
     total_train = sum([layer['trainable'] for layer in module_stack.values()])
@@ -86,4 +113,4 @@ def summarize(model: nn.Module, in_tensor: Union[List[torch.Tensor], torch.Tenso
     print(f'Total params: {total_train + total_nontrain:,}')
     print(f'Trainable params: {total_train:,}')
     print(f'Frozen params: {total_nontrain:,}')
-    print('=' * 90)
+    print('_' * 90)
